4f81c1a8faee031e3850dc95c51aa7064f8912d7
doraKoreny <dorakoreny@gmail.com>
2019-03-14 11:05:03 +0100

    added /getAll route, separated business logic to TodoService, corrected mistakes

--- src/main/java/com/dora/koreny/todoapp/App/controller/TodoController.java
+++ src/main/java/com/dora/koreny/todoapp/App/controller/TodoController.java
@@ -1,9 +1,8 @@
 package com.dora.koreny.todoapp.App.controller;
 
-import com.dora.koreny.todoapp.App.model.Status;
 import com.dora.koreny.todoapp.App.model.Todo;
-import com.dora.koreny.todoapp.App.repository.TodoRepository;
 import com.dora.koreny.todoapp.App.service.TodoService;
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,61 +15,59 @@ public class TodoController {
     private static final String SUCCESS = "{\"success\":true}";
 
     @Autowired
-    private TodoRepository todoRepository;
-
-    @Autowired
     private TodoService todoService;
 
-    // List all todos X
-    @PostMapping(path = "list", headers = "Content-type=application/x-www-form-urlencoded")
+    //List all todos X
+    @GetMapping("/getAll")
     public List<Todo> getTodos() {
-        return todoRepository.findAll();
+        return todoService.getTodos();
+    }
+
+    // List todos by status
+    @PostMapping(path = "/list", headers = "Content-type=application/x-www-form-urlencoded")
+    public String getTodosByStatus(@RequestParam("status") String status) {
+        return todoService.getTodosByStatus(status);
     }
 
     // Add new X
-    @PostMapping(path = "addTodo", headers = "Content-type=application/x-www-form-urlencoded")
+    @PostMapping(path = "/addTodo", headers = "Content-type=application/x-www-form-urlencoded")
     public String addTodo(@RequestParam("todo-title") String title) {
         todoService.addTodo(title);
         return SUCCESS;
     }
 
     // Remove all completed
-    @DeleteMapping(path = "todos/completed")
+    @DeleteMapping(path = "/todos/completed")
     public String removeAllCompleted() {
-        List<Todo> todos = todoRepository.findAll();
-        for (Todo todo: todos) {
-            if (todo.getStatus() == Status.COMPLETE) {
-                todoRepository.delete(todo);
-            }
-        }
+        todoService.removeAllCompleted();
         return SUCCESS;
     }
 
 
 //     Toggle status by id DB-ben működik, de a sorrendet összecseréli, frontenden is, de a css nem változik
-    @PutMapping(path = "todos/{id}/toggle_status", headers = "Content-type=application/x-www-form-urlencoded")
+    @PutMapping(path = "/todos/{id}/toggle_status", headers = "Content-type=application/x-www-form-urlencoded")
     public String toggleStatus(@PathVariable("id") long id, @RequestParam(value = "status", required = false) String status) {
-        todoService.toggleById(id, status);
+        todoService.toggleStatus(id, status.equals("true"));
         return SUCCESS;
     }
 
     // Toggle all status, átállítja db-ben mmindet completedre de visszafelé nem működik, frontenden szintén nincs nyoma
-    @PutMapping(path = "todos/toggle_all", headers = "Content-type=application/x-www-form-urlencoded")
+    @PutMapping(path = "/todos/toggle_all", headers = "Content-type=application/x-www-form-urlencoded")
     public String toggleAll(@RequestParam("toggle-all") String completed) {
         todoService.toggleAll(completed);
         return SUCCESS;
     }
 
     // Remove by id X
-    @DeleteMapping(path = "todos/{id}")
+    @DeleteMapping(path = "/todos/{id}")
     public void deleteTodo(@PathVariable("id") long id) {
-        todoRepository.deleteById(id);
+        todoService.deleteTodo(id);
     }
 
     // Update by id X
-    @PutMapping(path = "todos/{id}", headers = "Content-type=application/x-www-form-urlencoded")
+    @PutMapping(path = "/todos/{id}", headers = "Content-type=application/x-www-form-urlencoded")
     public String updateTodo(@PathVariable("id") long id, @RequestParam("todo-title") String title) {
-        todoService.updateTodoById(id, title);
+        todoService.updateTodo(id, title);
         return SUCCESS;
 
     }
--- src/main/java/com/dora/koreny/todoapp/App/service/TodoService.java
+++ src/main/java/com/dora/koreny/todoapp/App/service/TodoService.java
@@ -3,14 +3,44 @@ package com.dora.koreny.todoapp.App.service;
 import com.dora.koreny.todoapp.App.model.Status;
 import com.dora.koreny.todoapp.App.model.Todo;
 import com.dora.koreny.todoapp.App.repository.TodoRepository;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TodoService {
 
     @Autowired
     private TodoRepository todoRepository;
+
+    public List<Todo> getTodos() {
+        return todoRepository.findAll();
+    }
+
+    public String getTodosByStatus(String status) {
+        List<Todo> todos;
+        if (status == null || status.isEmpty()) {
+            todos = todoRepository.findAll();
+        } else {
+            todos = todoRepository.findAll()
+                    .stream()
+                    .filter(todo -> todo.getStatus().toString().equals(status.toUpperCase()))
+                    .collect(Collectors.toList());
+        }
+        JSONArray array = new JSONArray();
+        for (Todo todo : todos) {
+            JSONObject jo = new JSONObject();
+            jo.put("id", todo.getId());
+            jo.put("title", todo.getTitle());
+            jo.put("completed", todo.isComplete());
+            array.put(jo);
+        }
+        return array.toString(2);
+    }
 
     public void addTodo(String title) {
         Todo todo = Todo.builder()
@@ -20,15 +50,24 @@ public class TodoService {
         todoRepository.saveAndFlush(todo);
     }
 
-    public void toggleById(long id, String status) {
+
+    public void removeAllCompleted() {
+        List<Todo> todos = todoRepository.findAll();
+        for (Todo todo: todos) {
+            if (todo.getStatus() == Status.COMPLETE) {
+                todoRepository.delete(todo);
+            }
+        }
+    }
+
+    public void toggleStatus(long id, boolean isComplete) {
         Todo todo = todoRepository.findById(id).orElseThrow(() -> new IllegalStateException("Todo not found"));
-        if (!todo.isComplete() == status.equals("true")) {
+        if (isComplete) {
             todo.setStatus(Status.COMPLETE);
-            todoRepository.save(todo);
         } else {
             todo.setStatus(Status.ACTIVE);
-            todoRepository.save(todo);
         }
+        todoRepository.save(todo);
     }
 
     public void toggleAll(String completed) {
@@ -38,12 +77,15 @@ public class TodoService {
                 });
     }
 
-    public void updateTodoById(long id, String title) {
+    public void deleteTodo(long id) {
+        todoRepository.deleteById(id);
+    }
+
+    public void updateTodo(long id, String title) {
         todoRepository.findById(id)
                 .map(todo -> {todo.setTitle(title);
                     return todoRepository.save(todo);
                 });
     }
-
 }
 
