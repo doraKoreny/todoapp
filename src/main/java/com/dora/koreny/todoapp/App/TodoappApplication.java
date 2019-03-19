e05be5ca4118e24bccf46d4360bc0250aa01dd04
doraKoreny <dorakoreny@gmail.com>
2019-03-19 11:27:51 +0100

    Refactored TodoController and TodoappApplication, added auth0.properties.

--- src/main/java/com/dora/koreny/todoapp/App/TodoappApplication.java
+++ src/main/java/com/dora/koreny/todoapp/App/TodoappApplication.java
@@ -1,9 +1,18 @@
 package com.dora.koreny.todoapp.App;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@PropertySources({
+        @PropertySource("classpath:application.properties"),
+        @PropertySource("classpath:auth0.properties")
+})
 public class TodoappApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/dora/koreny/todoapp/App/controller/TodoController.java
+++ src/main/java/com/dora/koreny/todoapp/App/controller/TodoController.java
@@ -2,8 +2,8 @@ package com.dora.koreny.todoapp.App.controller;
 
 import com.dora.koreny.todoapp.App.model.Todo;
 import com.dora.koreny.todoapp.App.service.TodoService;
-import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,13 +24,13 @@ public class TodoController {
     }
 
     // List todos by status
-    @PostMapping(path = "/list", headers = "Content-type=application/x-www-form-urlencoded")
+    @PostMapping(path = "/list", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public String getTodosByStatus(@RequestParam("status") String status) {
         return todoService.getTodosByStatus(status);
     }
 
     // Add new X
-    @PostMapping(path = "/addTodo", headers = "Content-type=application/x-www-form-urlencoded")
+    @PostMapping(path = "/addTodo", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public String addTodo(@RequestParam("todo-title") String title) {
         todoService.addTodo(title);
         return SUCCESS;
@@ -43,16 +43,15 @@ public class TodoController {
         return SUCCESS;
     }
 
-
-//     Toggle status by id DB-ben működik, de a sorrendet összecseréli, frontenden is, de a css nem változik
-    @PutMapping(path = "/todos/{id}/toggle_status", headers = "Content-type=application/x-www-form-urlencoded")
+    // Toggle status
+    @PutMapping(path = "/todos/{id}/toggle_status", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public String toggleStatus(@PathVariable("id") long id, @RequestParam(value = "status", required = false) String status) {
         todoService.toggleStatus(id, status);
         return SUCCESS;
     }
 
-    // Toggle all status, átállítja db-ben mmindet completedre de visszafelé nem működik, frontenden szintén nincs nyoma
-    @PutMapping(path = "/todos/toggle_all", headers = "Content-type=application/x-www-form-urlencoded")
+    // Toggle all todos
+    @PutMapping(path = "/todos/toggle_all", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public String toggleAll(@RequestParam("toggle-all") String completed) {
         todoService.toggleAll(completed);
         return SUCCESS;
@@ -65,7 +64,7 @@ public class TodoController {
     }
 
     // Update by id X
-    @PutMapping(path = "/todos/{id}", headers = "Content-type=application/x-www-form-urlencoded")
+    @PutMapping(path = "/todos/{id}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public String updateTodo(@PathVariable("id") long id, @RequestParam("todo-title") String title) {
         todoService.updateTodo(id, title);
         return SUCCESS;
